f1f9f955a1c2816635173b19ce4ee300510b8a7f
Lee <lijinzhe@tenez.cn>
2022-06-29 22:45:31 +0800

    根据请求解析租户ID

---
+++ ueueo-framework/ueueo-web-multi-tenancy/src/main/java/Abp/AspNetCore/MultiTenancy/DomainTenantResolveContributor.java
@@ -0,0 +1,38 @@
+﻿package Abp.AspNetCore.MultiTenancy;
+
+//TODO: Create a better domain format. We can accept regex for example.
+
+import com.ueueo.multitenancy.ITenantResolveContext;
+import org.springframework.lang.NonNull;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DomainTenantResolveContributor extends HttpTenantResolveContributorBase {
+
+    public final String ContributorName = "Domain";
+
+    private final Pattern pattern;
+
+    @Override
+    public String getName() {
+        return ContributorName;
+    }
+
+    public DomainTenantResolveContributor(String domainRegex) {
+        pattern = Pattern.compile(domainRegex);
+    }
+
+    @Override
+    protected String getTenantIdOrNameFromHttpContextOrNull(@NonNull ITenantResolveContext context, @NonNull ServletRequestAttributes requestAttributes) {
+        String hostName = requestAttributes.getRequest().getServerName();
+        Matcher matcher = pattern.matcher(hostName);
+        context.setHandled(true);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return null;
+    }
+
+}
---
+++ ueueo-framework/ueueo-web-multi-tenancy/src/main/java/Abp/AspNetCore/MultiTenancy/QueryStringTenantResolveContributor.java
@@ -0,0 +1,43 @@
+﻿package Abp.AspNetCore.MultiTenancy;
+
+import com.ueueo.multitenancy.ITenantResolveContext;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.lang.NonNull;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class QueryStringTenantResolveContributor extends HttpTenantResolveContributorBase {
+
+    public final String ContributorName = "QueryString";
+
+    private AbpAspNetCoreMultiTenancyOptions options;
+
+    public QueryStringTenantResolveContributor(AbpAspNetCoreMultiTenancyOptions options) {
+        this.options = options;
+    }
+
+    @Override
+    public String getName() {
+        return ContributorName;
+    }
+
+    @Override
+    protected String getTenantIdOrNameFromHttpContextOrNull(@NonNull ITenantResolveContext context, @NonNull ServletRequestAttributes httpContext) {
+        if (StringUtils.isNotBlank(httpContext.getRequest().getQueryString())) {
+            String tenantKey = options.getTenantKey();
+            String[] queries = httpContext.getRequest().getQueryString().split("&");
+            for (String query : queries) {
+                String[] kv = query.split("=");
+                if (kv.length == 2 && tenantKey.equals(kv[0])) {
+                    String tenantValue = kv[1];
+                    if (StringUtils.isBlank(tenantValue)) {
+                        context.setHandled(true);
+                        return null;
+                    } else {
+                        return tenantValue;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
